a1bfe8ea9771480cece506c6f431e796d474861b
liang <709958894@qq.com>
2018-07-13 23:17:54 +0800

    edit aver.py

--- averagen.py
+++ averagen.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-N = 10 
+N = 3
 sum = 0 
 count = 0 
-print("please input 10 number: ")
-while conut < N: 
+print("please input 3 number: ")
+while count < N: 
     number = float(input())
     sum = sum + number
     count = count + 1 
